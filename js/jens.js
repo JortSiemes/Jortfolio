7d84f5216da3f7b9efeb25059061f7986efb361f
Jort Siemes <76953925+JortSiemes@users.noreply.github.com>
2023-01-30 11:34:27 +0100

    Update

--- js/jens.js
+++ js/jens.js
@@ -1,13 +1,2 @@
-let btnDownload = document.querySelector('#jensbutton');
+// hoi jens hoe
 
-
-// Must use FileSaver.js 2.0.2 because 2.0.3 has issues.
-btnDownload.addEventListener('click', () => {
-    let imagePath = "../Images/OneThousandAndOneNights.png";
-    let fileName = getFileName(imagePath);  
-    saveAs(imagePath, fileName);
-});
-
-function getFileName(str) {
-    return str.substring(str.lastIndexOf('/') + 1)
-}
